404b1b3a2c282b3516c22f2c9f4baf1c41d44397
DiegoICen <diego.i.cen.glez@gmail.com>
2022-05-21 12:24:12 -0500

    feat: Add git action configuration to aws

--- handler.js
+++ handler.js
@@ -8,13 +8,24 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const USERS_TABLE = process.env.USERS_TABLE;
+const IS_OFFLINE = !!process.env.IS_OFFLINE;
+const ONE_MINUTE = 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
+const ONE_WEEK = ONE_DAY * 7;
 
-const USERS_TABLE = process.env.USERS_TABLE
+if(IS_OFFLINE){
+  dynamoDB = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  });
 
-dynamoDB = new AWS.DynamoDB.DocumentClient({
-  region: 'localhost',
-  endpoint: 'http://localhost:8000'
-});
+} else{
+  dynamoDB = new AWS.DynamoDB.DocumentClient();
+}
+
+
 
 
 app.get('/users', (req, res) => {
@@ -48,6 +59,35 @@ app.post('/user', (req, res) => {
   })
 
   
+});
+
+app.get('/user/:userId', (req, res) => {
+  const{
+    userId,
+  } = req.params;
+
+  /*const inCachUser = redis.get(`user-${userId}`)
+
+  if(inCache){
+    return res.send(JSON.parse(inCachUser));
+  }*/
+
+  const param = {
+
+    TableName: USERS_TABLE,
+    Item: {
+      userId,
+    }
+  }
+
+  /*dynamoDB.get(params, (error, result) => {
+    if(result.Item){   
+      const user = result.Item;
+      redis.sent(`user-${userId}`, user, ONE_MINUTE);
+      return res.send(user)
+    }
+  })  */
+
 });
 
 module.exports.generic = serverless(app);
